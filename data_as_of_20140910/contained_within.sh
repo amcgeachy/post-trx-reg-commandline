f6bb490e2caf4f2e42b34a83530d415da255cae4
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-02 17:15:05 -0800

    seems the output wasn't getting sorted before going to uniq, which meant that things that weren't RIGHT next to each other weren't being properly combined into a single fragment with multiple counts. hopefully this fixes it.

--- data_as_of_20140910/contained_within.sh
+++ data_as_of_20140910/contained_within.sh
@@ -3,11 +3,11 @@
 for sample in up down no_recomb post_recomb
 
 do
-    bedtools intersect -a ${sample}_tot_sorted.bed -b saccer3.bed -wao -s -f 1 > ${sample}_inside_orf.bed
+    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_sorted.bed -b saccer3.bed > ${sample}_inside_orf_new.bed
 done
 
 for sample in up down no_recomb post_recomb
 
 do
-    cut -f1-3,6-19 ${sample}_inside_orf.bed | uniq -c > ${sample}_orf_unique.bed
+    cut -f1-3,6-19 ${sample}_inside_orf_new.bed | uniq -c > ${sample}_orf_unique_new.bed
 done
