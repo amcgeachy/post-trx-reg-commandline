2896e150e95740d86acee2be89784d90ba136c17
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-03 13:51:13 -0800

    adding the 'sortout' option doesn't seem to fix it. going to try doing command line sort.

--- data_as_of_20140910/contained_within.sh
+++ data_as_of_20140910/contained_within.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+#for sample in up down no_recomb post_recomb
+#
+#do
+#    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_sorted.bed -b saccer3.bed > ${sample}_inside_orf_new.bed
+#done
+
 for sample in up down no_recomb post_recomb
 
 do
-    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_sorted.bed -b saccer3.bed > ${sample}_inside_orf_new.bed
-done
-
-for sample in up down no_recomb post_recomb
-
-do
-    cut -f1-3,6-19 ${sample}_inside_orf_new.bed | uniq -c > ${sample}_orf_unique_new.bed
+    sort ${sample}_inside_orf_new.bed | cut | uniq -c > ${sample}_orf_unique_new2.bed
 done
