d46f1f01065b65cc8e16292fb46ee282bb3dd2d7
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-03 17:35:25 -0800

    so the reason things were still occurring multiple times post adding a sort even was that there were identifical fragments with different mapping scores (ranging ~0-42.). we don't really care about the scores unless they're really bad, so we're going to cut out the bad ones(MQ<20, or 1% chance wrongly mapped), then remove the score so we can collapse things using uniq

--- data_as_of_20140910/unique_and_contained_within.sh
+++ data_as_of_20140910/unique_and_contained_within.sh
@@ -3,14 +3,15 @@
 for sample in up down no_recomb post_recomb
 
 do
-    cut -f1-3,5-6 ${sample}_tot_sorted.bed | uniq -c | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $5 "\t" $6}' > ${sample}_tot_unique.bed
+    awk '{if ($5>20) {print}}' ${sample}_tot_sorted.bed > ${sample}_quality.bed
+    cut -f1-3,6 ${sample}_quality.bed | sort | uniq -c | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $5 "\t" $6}' > ${sample}_tot_unique.bed
 done
 
-for sample in up down no_recomb post_recomb
-
-do
-    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_unique.bed -b saccer3.bed > ${sample}_inside_orf_unique.bed
-done
+#for sample in up down no_recomb post_recomb
+#
+#do
+#    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_unique.bed -b saccer3.bed > ${sample}_inside_orf_unique.bed
+#done
 
 #for sample in up down no_recomb post_recomb
 #
