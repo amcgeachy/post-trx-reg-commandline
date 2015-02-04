f8769582fdb38b977d189e5b8e05e8db5dafdcbb
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-03 16:16:32 -0800

    well, it's not properly intersecting. so something is the matter. let's figure that out first.

--- data_as_of_20140910/unique_and_contained_within.sh
+++ data_as_of_20140910/unique_and_contained_within.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#for sample in up down no_recomb post_recomb
-#
-#do
-#    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_sorted.bed -b saccer3.bed > ${sample}_inside_orf_new.bed
-#done
+for sample in up down no_recomb post_recomb
+
+do
+    cut -f1-3,5-6 ${sample}_tot_sorted.bed | uniq -c > ${sample}_tot_unique.bed
+done
 
 for sample in up down no_recomb post_recomb
 
 do
-    cut -f1-3,6-19 ${sample}_inside_orf_new.bed | sort | uniq -c > ${sample}_orf_unique_new3.bed
+    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_unique.bed -b saccer3.bed > ${sample}_inside_orf_unique.bed
 done
+
+#for sample in up down no_recomb post_recomb
+#
+#do
+#    cut -f1-3,6-19 ${sample}_inside_orf_new.bed | sort | uniq -c > ${sample}_orf_unique_new3.bed
+#done
