1c5d01c90ed7833df10e574c5cd3be8a603a155e
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-03 19:21:17 -0800

    adding an arbitrary value to give a column 5 and put column 6 in the right place seems to have fixed it!

--- data_as_of_20140910/unique_and_contained_within.sh
+++ data_as_of_20140910/unique_and_contained_within.sh
@@ -4,7 +4,7 @@ for sample in up down no_recomb post_recomb
 
 do
     awk '{if ($5>20) {print}}' ${sample}_tot_sorted.bed > ${sample}_quality.bed
-    cut -f1-3,6 ${sample}_quality.bed | sort | uniq -c | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $5 "\t" $6}' > ${sample}_tot_unique.bed
+    cut -f1-3,6 ${sample}_quality.bed | sort | uniq -c | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" "100" "\t" $5}' > ${sample}_tot_unique.bed
 done
 
 for sample in up down no_recomb post_recomb
