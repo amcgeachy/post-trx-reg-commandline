7b79d180cfb58e593eda8aff42bbaeecb16d63bc
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-02-03 18:19:06 -0800

    made some changes to get the filtered, but obviously still having a problem. suspect its a not having a column five

--- data_as_of_20140910/unique_and_contained_within.sh
+++ data_as_of_20140910/unique_and_contained_within.sh
@@ -7,14 +7,8 @@ do
     cut -f1-3,6 ${sample}_quality.bed | sort | uniq -c | awk '{print $2 "\t" $3 "\t" $4 "\t" $1 "\t" $5 "\t" $6}' > ${sample}_tot_unique.bed
 done
 
-#for sample in up down no_recomb post_recomb
-#
-#do
-#    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_unique.bed -b saccer3.bed > ${sample}_inside_orf_unique.bed
-#done
+for sample in up down no_recomb post_recomb
 
-#for sample in up down no_recomb post_recomb
-#
-#do
-#    cut -f1-3,6-19 ${sample}_inside_orf_new.bed | sort | uniq -c > ${sample}_orf_unique_new3.bed
-#done
+do
+    bedtools intersect -wao -sortout -s -f 1 -a ${sample}_tot_unique.bed -b saccer3.bed > ${sample}_inside_orf_unique.bed
+done
