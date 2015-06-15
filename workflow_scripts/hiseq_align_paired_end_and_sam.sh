19cf88c579f99c75b60474d310b65892fe105125
Anna McGeachy <anna.mcgeachy@gmail.com>
2015-06-14 18:08:52 -0700

    I updated the align_and_sam to work from a remote folder (scripts folder) and to deal with hiseq data in a new file, hiseq_align_and_sam. I updated the workflow.txt to reflect this.

--- workflow_scripts/hiseq_align_paired_end_and_sam.sh
+++ workflow_scripts/hiseq_align_paired_end_and_sam.sh
@@ -1,13 +1,47 @@
-for R1 in ` ls *_R1_*`
+#!/bin/bash
 
-do
-sample=`echo ${R1} | cut -d_ -f1 | sed s/NIAM005// `
-echo ${sample}
+echo 'What directory are the files located in (use absolute path)?'
+read proj_path
 
-R2=`echo ${R1} | sed s/R1/R2/`
+echo 
+echo ${proj_path}
 
-echo ${R1},${R2},${sample}
+echo 'Where do you want the completed files to go? (use absolute path)'
+read dest_path
+if [ -d ${dest_path} ]; then
+ echo ${dest_path}
+ else 
+  mkdir ${dest_path}
+ echo ${dest_path}
+fi
+echo
 
-bowtie2 -p10 -x ~/saccer_w_VoBFP --un-conc ${sample}_unaligned.fq -1 ${R1} -2 ${R2} -S ${sample}.sam
+for names in `ls ${proj_path}/`
+
+ do echo ${names}
+ samples=`echo ${names} | cut -d_ -f3`
+ echo "Current sample: ${samples}"
+
+ for R1 in `ls ${proj_path}/${names}/*_R1_*`
+  do echo ``
+  R1_list=''
+  R1_list="${R1_list},${R1}"
+
+  R2_list=''
+  R2=`echo ${R1} | sed s/R1/R2/`
+  R2_list="${R2_list},${R2}"
+# R2_list=`paste ${R2} -d, -s`
+# echo ${R2_list}
+ 
+# echo ${R1},${R2},${samples}
+ done
+ R1_list=`echo ${R1_list} | sed -E 's/,//'`
+# echo ${R1_list}
+# echo ''
+ R2_list=`echo ${R2_list} | sed -E 's/,//'`
+# echo ${R2_list}
+ echo "I am trying to do that thing."
+ echo ""
+ bowtie2 -p10 -x ~/saccer_w_VoBFP/saccer_w_VoBFP --un-conc ${dest_path}/${samples}_unaligned.fq -1 ${R1_list} -2 ${R2_list} -S ${dest_path}/${samples}.sam
 done
 
